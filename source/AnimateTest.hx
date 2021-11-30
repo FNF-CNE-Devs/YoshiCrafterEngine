f28583130028f67b39ce9d9b3271493749f0205b
YoshiCrafter29 <yoshicrafter.mkpc@gmail.com>
2021-11-30 22:56:08 +0100

    Update AnimateTest.hx

--- source/AnimateTest.hx
+++ source/AnimateTest.hx
@@ -4,7 +4,7 @@ import flixel.FlxState;
 class AnimateTest extends FlxState {
     public override function create() {
         new Paths_Mod("Friday Night Funkin'").getAnimateManager("cutscenes/tightBars/", function(f) {
-            add(f.createAnimation("TANK TALK 2"));
+            add(f.("TANK TALK 2").child);
         });
 
     }
