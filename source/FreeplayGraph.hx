f88c283559c6cc2b0bb9f09bef2ec4e9e9c9a1a5
YoshiCrafter29 <yoshicrafter.mkpc@gmail.com>
2021-12-09 15:58:32 +0100

    TRYNA FIX

--- source/FreeplayGraph.hx
+++ source/FreeplayGraph.hx
@@ -18,7 +18,7 @@ class FreeplayGraph {
         bData.drawEllipse(center, width - 2, height - 2, FlxColor.BLACK, false, 2);
         var current:Float = 0;
         for (g in data) {
-            var pos = new FlxPoint(center.x + (Math.sin(current / total * maxAngle) * (height / 2)), center.y + (Math.sin(current / total * maxAngle) * (height / 2)));
+            var pos = new FlxPoint(center.x + (Math.sin(current / total * maxAngle) * (width / 2)), center.y + (Math.sin(current / total * maxAngle) * (height / 2)));
             bData.drawLine(center, pos, FlxColor.BLACK, 1.5);
             current += g.number;
         }
