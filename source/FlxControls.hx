c3eae01fd4b4fcd8bcfceb350244d0d47cb7513a
YoshiCrafter29 <yoshicrafter.mkpc@gmail.com>
2022-02-03 10:43:56 +0100

    Added HaxeDevelop project

---
+++ source/FlxControls.hx
@@ -0,0 +1,18 @@
+
+
+import flixel.input.keyboard.FlxKeyList;
+import flixel.input.keyboard.FlxKey;
+
+using haxe.EnumTools;
+
+class FlxControls {
+    public static var pressed(get, null):Dynamic;
+    private static function get_pressed() {
+        var list = new Dynamic();
+        // Reflect.
+        
+    }
+    public static function anyPressed(keys:Array<FlxKey>) {
+
+    }
+}
